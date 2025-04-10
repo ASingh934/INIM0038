e43ce2cdd7d30b30860621191d03b58bab0551ff
ASingh934 <21prepmaster@gmail.com>
2025-04-10 18:45:17 +0100

    Update 1. DataPrep.R

--- 1. DataPrep.R
+++ 1. DataPrep.R
@@ -24,6 +24,7 @@ library(caret)
 library(UpSetR)
 library(ComplexUpset)
 library(patchwork)
+library(XGR)
 
 gene_data <- read.csv("C:/Users/ABSin/Downloads/BIOAID_tpm_PC0.001_log2_genesymbol_dedup (1).csv")
 clinical_data <- readRDS("C:/Users/ABSin/Downloads/BioAID_resp_master_v2_March25.rds")
